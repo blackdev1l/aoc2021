c4db031990f8e3cd7416331751895ab0a8c3f5ab
Cristian Achille <blackdev1l@autistici.org>
2021-12-06 22:35:56 +0100

    d6p1 cleaned

--- day6/day6.go
+++ day6/day6.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/blackdev1l/aoc2021/utils"
 )
@@ -25,26 +24,27 @@ func cycle(list *[]int) {
 }
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(5)
+	// var wg sync.WaitGroup
+	// wg.Add(5)
 	list := parseInput()
-	L1 := list[0:50]
-	L2 := list[50:100]
-	L3 := list[100:150]
-	L4 := list[150:200]
-	L5 := list[200:300]
-	runFor(256, &wg, L1)
-	runFor(256, &wg, L2)
-	runFor(256, &wg, L3)
-	runFor(256, &wg, L4)
-	runFor(256, &wg, L5)
+	// L1 := list[0:50]
+	// L2 := list[50:100]
+	// L3 := list[100:150]
+	// L4 := list[150:200]
+	// L5 := list[200:300]
+	// runFor(256, &wg, L1)
+	// runFor(256, &wg, L2)
+	// runFor(256, &wg, L3)
+	// runFor(256, &wg, L4)
+	// runFor(256, &wg, L5)
 
-	wg.Wait()
-	fmt.Printf("fishes are %v\n", len(L1)+len(L2))
+	runFor(256, list)
+
+	// wg.Wait()
+	fmt.Printf("fishes are %v\n", len(list))
 }
 
-func runFor(days int, wg *sync.WaitGroup, list []int) {
-	defer wg.Done()
+func runFor(days int, list []int) {
 	for i := 1; i < days+1; i++ {
 		fmt.Printf("after %v days\n ", i)
 		cycle(&list)
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -9,8 +9,8 @@ var example = []int{3, 4, 3, 1, 2}
 
 func TestCount(t *testing.T) {
 	fmt.Printf("Initial state: %v\n", example)
-	for i := 1; i < 19; i++ {
-		fmt.Printf("after %v days ", i)
+	for i := 1; i < 257; i++ {
+		fmt.Printf("after %v days \n", i)
 
 		cycle(&example)
 	}
